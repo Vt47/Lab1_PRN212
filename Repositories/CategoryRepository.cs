e033b10962d350f0aeb87a53be15e03eae00200d
Vt47 <havietthanh3003@gmail.com>
2025-06-05 16:35:30 +0700

    Add project files.

---
+++ Repositories/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using BusinessObjects;
+using DataAccessLayer;
+
+namespace Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public List<Category> GetCategories()
+            => CategoryDAO.GetCategories();
+    }
+}
